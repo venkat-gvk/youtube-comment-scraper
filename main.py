2804814e7db7388ad08a92d8e90e57621d3026e6
venkat-gvk <gvk496@gmail.com>
2021-08-26 22:57:52 +0530

    fixed typo

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from Scraper import Scraper
 from GenerateReport import GenerateReport
 
-youtube = Scraper("https://www.youtube.com/watch?v=wKjx8QS3OCY")
+youtube = Scraper("https://www.youtube.com/watch?v=aJRbPG0aSHc")
 commentsAndReplies = youtube.run()
 
 GenerateReport(youtube.channelName, youtube.current_url,
                youtube.channelComments, commentsAndReplies)
+

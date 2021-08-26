d55c513c029df90d04c335b66b082a410aa62189
venkat-gvk <gvk496@gmail.com>
2021-08-26 23:19:53 +0530

    added __main__ guard

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from Scraper import Scraper
 from GenerateReport import GenerateReport
+import sys
 
-youtube = Scraper("https://www.youtube.com/watch?v=aJRbPG0aSHc")
-commentsAndReplies = youtube.run()
+SEARCH_LINK = ''
 
-GenerateReport(youtube.channelName, youtube.current_url,
+def start():
+    global SEARCH_LINK
+    SEARCH_LINK = input('Enter youtube link: ').strip()
+
+    if SEARCH_LINK.find('youtube') == -1:
+        print("invalid youtube link")
+        print('exiting...')
+
+        sys.exit()
+
+
+if __name__ == '__main__':
+    start()
+    youtube = Scraper(SEARCH_LINK)
+    commentsAndReplies = youtube.run()
+
+    if not commentsAndReplies:
+        sys.exit()
+    
+    GenerateReport(youtube.channelName, youtube.current_url,
                youtube.channelComments, commentsAndReplies)
 
